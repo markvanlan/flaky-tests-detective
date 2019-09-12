8288d94d01166ec5715dbcc46d62ae12a75a8308
romanrizzi <rizziromanalejandro@gmail.com>
2019-09-12 11:05:15 -0300

    Use Markdown printer when printing reports

--- print_report.rb
+++ print_report.rb
@@ -2,7 +2,7 @@
 
 # !/usr/bin/env ruby
 require_relative 'lib/detective.rb'
-require_relative 'lib/printers/text_printer.rb'
+require_relative 'lib/printers/markdown_printer.rb'
 require_relative 'lib/archives/file_system_archive.rb'
 
 report_path = ARGV[0]
@@ -12,4 +12,4 @@ threshold = 3
 working_dir = File.expand_path('../reports', __FILE__)
 archive = FileSystemArchive.new(working_dir, report_filename)
 
-puts Detective.new.report_for(TextPrinter.new, threshold, archive)
+puts Detective.new.report_for(MarkdownPrinter.new, threshold, archive)
